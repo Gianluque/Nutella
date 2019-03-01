6719e5c8fe9c556188c62df1f6e426259de02a54
githubdemarco <48102206+githubdemarco@users.noreply.github.com>
2019-03-01 18:21:50 -0400

    Add files via upload

---
+++ Untitled2.m
@@ -0,0 +1,14 @@
+
+s=100;
+x=[-5:5];
+y=uint8([0 0 0 0 0 0 0 0 0 0 0]);
+while s
+    y=circshift(y,-1);
+    y(11)=uint8(128*10*rand);             %lee un valor del puerto le puse un random para generar bytes de prueba
+    plot(x,y);
+    drawnow
+    grid on;
+    yticklabels({})%sin valores en los ejes
+    xticklabels({})
+    s=s-1;
+end
